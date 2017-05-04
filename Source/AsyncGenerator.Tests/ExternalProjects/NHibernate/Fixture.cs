21db248d6f576d47ca02c86089d0d7eee99e0c24
Alexander Zaytsev <hazzik@gmail.com>
2017-05-04 17:39:28 +1200

    Ignore some of the NHibernate methods

--- Source/AsyncGenerator.Tests/ExternalProjects/NHibernate/Fixture.cs
+++ Source/AsyncGenerator.Tests/ExternalProjects/NHibernate/Fixture.cs
@@ -60,12 +60,103 @@ namespace AsyncGenerator.Tests.ExternalProjects.NHibernate
 						return MethodConversion.Ignore;
 					}
 					break;
+				case "SqlStatementLogger": //Generated because of Console.WriteLineAsync
+					return MethodConversion.Ignore;
+				case "IInterceptor": //We need to adjust proxy code generation to call IInterceptor async
+				case "DefaultDynamicLazyFieldInterceptor": //The implementor of IInterceptor
+				case "DefaultLazyInitializer": //The implementor of IInterceptor
+				case "IFieldInterceptor": //Is called by DefaultDynamicLazyFieldInterceptor.Intercept
+				case "AbstractFieldInterceptor":// The implementor of IFieldInterceptor
+					if (symbol.Name == "Intercept" || 
+						symbol.Name == "InitializeOrGetAssociation"//Private method of AbstractFieldInterceptor
+						) 
+					{
+						return MethodConversion.Ignore;
+					}
+					break;
+				case "ILazyPropertyInitializer": // Is called by AbstractFieldInterceptor.Intercept
+				case "AbstractEntityPersister":
+					if (symbol.Name == "InitializeLazyProperty" || 
+						symbol.Name == "InitializeLazyPropertiesFromDatastore" ||
+						symbol.Name == "InitializeLazyPropertiesFromCache")
+					{
+						return MethodConversion.Ignore;
+					}
+					break;
+				case "BasicLazyInitializer": // Is called by DefaultLazyInitializer.Intercept
+					if (symbol.Name == "Invoke")
+					{
+						return MethodConversion.Ignore;
+					}
+					break;
+				case "SqlGenerator":
+				case "HqlLexer":
+					if (symbol.Name == "EvaluatePredicate")
+					{
+						// Generated because of Console.WriteLineAsync
+						return MethodConversion.Ignore;
+					}
+					break;
+				case "StatefulPersistenceContext":
+					if (
+						symbol.Name == "SetReadOnly" || // Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
+						symbol.Name == "Unproxy" // Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
+						)
+					{
+					
+						return MethodConversion.Ignore;
+					}
+					break;
+
+				case "CollectionType":
+					if (symbol.Name == "Contains")
+					{
+						// Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
+						return MethodConversion.Ignore;
+					}
+					break;
+
+				case "NHibernateProxyHelper":
+					if (symbol.Name == "GuessClass")
+					{
+						// Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
+						return MethodConversion.Ignore;
+					}
+					break;
+
+				case "NHibernateUtil":
+					if (symbol.Name == "IsPropertyInitialized")
+					{
+						// Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
+						return MethodConversion.Ignore;
+					}
+					break;
+
+				case "StatelessSessionImpl": // Probably bug here.
+				case "SessionImpl":
+					if (symbol.Name == "BestGuessEntityName" || 
+						symbol.Name == "Contains")
+					{
+						// Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
+						return MethodConversion.Ignore;
+					}
+					break;
+
+				case "UnsavedValueFactory":
+					if (symbol.Name == "GetUnsavedVersionValue")
+					{
+						//The Seed method that was recognized to be async is never used as async.
+						return MethodConversion.Ignore;
+					}
+					break;
+
 				case "IBatcher":
 					if (symbol.Name == "ExecuteReader" || symbol.Name == "ExecuteNonQuery")
 					{
 						return MethodConversion.ToAsync;
 					}
 					break;
+
 				case "IAutoFlushEventListener":
 				case "IFlushEventListener":
 				case "IDeleteEventListener":
@@ -81,6 +172,7 @@ namespace AsyncGenerator.Tests.ExternalProjects.NHibernate
 				case "IPreCollectionUpdateEventListener":
 				case "IPreDeleteEventListener":
 				case "IPreInsertEventListener":
+				case "IPreLoadEventListener":
 				case "IPreUpdateEventListener":
 					return MethodConversion.ToAsync;
 			}
