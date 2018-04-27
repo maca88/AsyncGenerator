4843cfd06d883bdea7818ec8b701d221d5b78a14
Eamon Hetherton <eamonhetherton@gmail.com>
2018-04-27 11:57:33 +1000

    Test fixture to demonstrate issue #90

---
+++ Source/AsyncGenerator.Tests/ExceptionHandling/Input/PropagateOperationCanceledException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AsyncGenerator.TestCases;
+
+namespace AsyncGenerator.Tests.ExceptionHandling.Input
+{
+	public class PropagateOperationCanceledException
+	{
+		public void MethodThatCatchesExceptions()
+		{
+			try
+			{
+				SimpleFile.Read();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("My wrapped exception", ex);
+			}
+		}
+	}
+}
