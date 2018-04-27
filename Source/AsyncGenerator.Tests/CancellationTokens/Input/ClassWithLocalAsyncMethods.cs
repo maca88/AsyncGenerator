284cc23fc06aa381e4cf177f4dcd4a1925023fe7
Eamon Hetherton <eamonhetherton@gmail.com>
2018-04-27 11:42:05 +1000

    Test Fixutre demonstraing issue #91

---
+++ Source/AsyncGenerator.Tests/CancellationTokens/Input/ClassWithLocalAsyncMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncGenerator.Tests.CancellationTokens.Input
+{
+	public class ClassWithLocalAsyncMethods
+	{
+		public void Work()
+		{
+			LocalMethodWithCancellation();
+			LocalMethodWithoutCancellation();
+			LocalMethodWithCancellation();
+		}
+
+		public void LocalMethodWithCancellation()
+		{
+			Thread.Sleep(100);
+		}
+		public void LocalMethodWithoutCancellation()
+		{
+			Thread.Sleep(100);
+		}
+
+		public async Task LocalMethodWithCancellationAsync(CancellationToken cancellationToken)
+		{
+			await Task.Delay(100, cancellationToken);
+		}
+		public async Task LocalMethodWithoutCancellationAsync()
+		{
+			await Task.Delay(100);
+		}
+	}
+}
