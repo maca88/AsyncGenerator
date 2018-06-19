7205d74b88a7201f3593e6fcb01b47a90cf559a8
maca88 <bostjan.markezic@siol.net>
2018-06-19 21:20:22 +0200

    Fixed cancellation token not being propagated correctly on derived types, fixes #107

---
+++ Source/AsyncGenerator.Tests/CancellationTokens/Input/AbstractClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsyncGenerator.TestCases;
+
+namespace AsyncGenerator.Tests.CancellationTokens.Input
+{
+	public abstract class AbstractClass
+	{
+		public virtual void Read()
+		{
+			SimpleFile.Read();
+		}
+
+		public virtual bool Write(string value)
+		{
+			return SimpleFile.Write(value);
+		}
+
+		public abstract void Clear();
+	}
+
+	public class DerivedClass : AbstractClass
+	{
+		public override void Read()
+		{
+			base.Read();
+		}
+
+		public override bool Write(string value)
+		{
+			return base.Write(value);
+		}
+
+		public override void Clear()
+		{
+			SimpleFile.Read();
+		}
+	}
+}
