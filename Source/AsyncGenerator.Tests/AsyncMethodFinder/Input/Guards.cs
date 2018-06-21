2474713441ceeb6ff9614bba6f538d099443f688
maca88 <bostjan.markezic@siol.net>
2018-06-21 21:17:55 +0200

    Avoid generating two guards for the first statement, fixes #110

---
+++ Source/AsyncGenerator.Tests/AsyncMethodFinder/Input/Guards.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsyncGenerator.TestCases;
+
+namespace AsyncGenerator.Tests.AsyncMethodFinder.Input
+{
+	public class Guards
+	{
+		public void Test()
+		{
+			SimpleFile.Write("");
+			SimpleFile.Write("");
+		}
+	}
+}
