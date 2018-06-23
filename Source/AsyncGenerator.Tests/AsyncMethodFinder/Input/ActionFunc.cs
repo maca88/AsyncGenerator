28b30e18eee2f30f991f34c877f3fd65be59e5e3
maca88 <bostjan.markezic@siol.net>
2018-06-23 22:14:45 +0200

    Fixed async counterpart with a delegate being generated when there is already one, fixes #111

---
+++ Source/AsyncGenerator.Tests/AsyncMethodFinder/Input/ActionFunc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AsyncGenerator.TestCases;
+
+namespace AsyncGenerator.Tests.AsyncMethodFinder.Input
+{
+	public class ActionFunc
+	{
+		public void Test()
+		{
+			Execute(val =>
+			{
+				SimpleFile.Read();
+				SimpleFile.Read();
+			});
+		}
+
+		private void Execute(Action<int> func)
+		{
+			SimpleFile.Read();
+		}
+
+		private Task ExecuteAsync(Func<int, Task> func)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
