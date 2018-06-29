157d5ed28f8c057179c3b6f8900108d577f883f5
maca88 <bostjan.markezic@siol.net>
2018-06-29 18:09:16 +0200

    Fixed FindNewlyInvokedMethodsWithAsyncCounterpart when traversing get and set accessors that contain an accessor call, fixes #112

---
+++ Source/AsyncGenerator.Tests/AsyncProperites/Input/GetAccessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsyncGenerator.TestCases;
+
+namespace AsyncGenerator.Tests.AsyncProperites.Input
+{
+	public interface IGetAccessor
+	{
+		bool Write { get; }
+#if TEST
+		Task<bool> GetWriteAsync();
+#endif
+	}
+
+	public class GetAccessor : IGetAccessor
+	{
+		private bool _value;
+		private bool _isBusy;
+
+		public bool Write
+		{
+			get
+			{
+				if (!IsBusy)
+				{
+					_value = SimpleFile.Write("");
+				}
+
+				return _value;
+			}
+		}
+
+		public bool IsBusy
+		{
+			get => _isBusy;
+			set => _isBusy = value;
+		}
+	}
+}
