4e1c41c6512703088f0310a7b9291f002ec066b4
maca88 <bostjan.markezic@siol.net>
2019-08-27 20:02:09 +0100

    Add support for C# 8 nullables (#134)

---
+++ Source/AsyncGenerator.Tests/NullableReferences/Fixture.cs
@@ -0,0 +1,47 @@
+﻿#if !NET461
+using System.Threading.Tasks;
+using AsyncGenerator.Configuration;
+using AsyncGenerator.Core;
+using Microsoft.CodeAnalysis.CSharp;
+using NUnit.Framework;
+
+namespace AsyncGenerator.Tests.NullableReferences
+{
+	[TestFixture]
+	public class Fixture : BaseFixture
+	{
+		[Test]
+		public async Task TestNullableRestoreDirective()
+		{
+			var ignore = false;
+			var config = AsyncCodeConfiguration.Create()
+				.ConfigureProject(GetTestProjectPath("NullableRestore"), p => p
+					.ConfigureAnalyzation(a => a
+						.MethodConversion(s => MethodConversion.Smart)
+						.CancellationTokens(true))
+					.ConfigureTransformation(t => t
+						.AfterTransformation(result =>
+						{
+							if (((CSharpCompilationOptions)result.Project.CompilationOptions).NullableContextOptions == NullableContextOptions.Disable)
+							{
+								ignore = true;
+								return;
+							}
+
+							AssertValidAnnotations(result);
+							Assert.AreEqual(1, result.Documents.Count);
+							var document = result.Documents[0];
+							Assert.NotNull(document.OriginalModified);
+							Assert.AreEqual(GetOutputFile("NullableRestore"), document.Transformed.ToFullString());
+						}))
+					.ApplyChanges(false));
+
+			await AsyncCodeGenerator.GenerateAsync(config);
+			if (ignore)
+			{
+				Assert.Ignore("Nullable is not enabled.");
+			}
+		}
+	}
+}
+#endif
---
+++ Source/AsyncGenerator/Transformation/Internal/RestoreNullableTransformer.cs
@@ -0,0 +1,49 @@
+﻿#if !NET461
+using System.Linq;
+using System.Threading.Tasks;
+using AsyncGenerator.Core.Configuration;
+using AsyncGenerator.Core.Plugins;
+using AsyncGenerator.Core.Transformation;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace AsyncGenerator.Transformation.Internal
+{
+	internal class RestoreNullableTransformer : IDocumentTransformer
+	{
+		private bool _enabled;
+
+		public Task Initialize(Project project, IProjectConfiguration configuration, Compilation compilation)
+		{
+			_enabled = ((CSharpCompilationOptions) project.CompilationOptions).NullableContextOptions != NullableContextOptions.Disable;
+			return Task.CompletedTask;
+		}
+
+		public CompilationUnitSyntax Transform(IDocumentTransformationResult transformationResult)
+		{
+			if (!_enabled)
+			{
+				return null;
+			}
+
+			var transformed = transformationResult.Transformed;
+			var firstNode = transformed.DescendantNodes().FirstOrDefault();
+			if (firstNode == null)
+			{
+				return null;
+			}
+
+			var directive = NullableDirectiveTrivia(
+				Token(SyntaxKind.HashToken),
+				Token(TriviaList(), SyntaxKind.NullableKeyword, TriviaList(Space)),
+				Token(SyntaxKind.RestoreKeyword),
+				Token(TriviaList(), SyntaxKind.EndOfDirectiveToken, TriviaList(transformationResult.EndOfLineTrivia)), true);
+
+			return transformed.ReplaceNode(firstNode,
+				firstNode.WithLeadingTrivia(firstNode.GetLeadingTrivia().Add(Trivia(directive))));
+		}
+	}
+}
+#endif
