a1a458b3cdc6ff2986618a7c814b1f311f912059
maca88 <bostjan.markezic@siol.net>
2021-12-02 22:15:17 +0100

    Add support for .NET 6 and remove netcoreapp2.1

--- Source/AsyncGenerator/Extensions/Internal/AssemblyExtensions.cs
+++ Source/AsyncGenerator/Extensions/Internal/AssemblyExtensions.cs
@@ -12,11 +12,7 @@ namespace AsyncGenerator.Extensions.Internal
 	{
 		public static string GetPath(this Assembly assembly)
 		{
-#if LEGACY
-			var path = assembly.CodeBase;
-#else
 			var path = assembly.Location;
-#endif
 			if (!Uri.TryCreate(path, UriKind.Absolute, out var uri))
 			{
 				throw new InvalidOperationException($"Invalid assembly location: '{assembly.Location}'");
